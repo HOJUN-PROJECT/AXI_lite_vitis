9ade2fc64bc3513ac96ff28c14539b3a59cbfb82
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-06 08:56:04 +0900

    Format GPIO header file for consistency

--- device/GPIO/gpio.h
+++ device/GPIO/gpio.h
@@ -4,27 +4,27 @@
 #include <stdint.h>
 
 typedef struct {
-   volatile uint32_t CR;
-   volatile uint32_t ODR;
-   volatile uint32_t IDR;
+	volatile uint32_t CR;
+	volatile uint32_t ODR;
+	volatile uint32_t IDR;
 }GPIO_TypeDef;
 
-#define   GPIO_0_BASEADDR    XPAR_GPIO_0_S00_AXI_BASEADDR
-#define   GPIO_1_BASEADDR    XPAR_GPIO_1_S00_AXI_BASEADDR
-#define   GPIO_2_BASEADDR    XPAR_GPIO_2_S00_AXI_BASEADDR
+#define	GPIO_0_BASEADDR 	XPAR_GPIO_0_S00_AXI_BASEADDR
+#define	GPIO_1_BASEADDR 	XPAR_GPIO_1_S00_AXI_BASEADDR
+#define	GPIO_2_BASEADDR 	XPAR_GPIO_2_S00_AXI_BASEADDR
 
 #define GPIOA ((GPIO_TypeDef *) GPIO_0_BASEADDR)
 #define GPIOB ((GPIO_TypeDef *) GPIO_1_BASEADDR)
 #define GPIOC ((GPIO_TypeDef *) GPIO_2_BASEADDR)
 
-#define GPIO_PIN_0   0
-#define GPIO_PIN_1   1
-#define GPIO_PIN_2   2
-#define GPIO_PIN_3   3
-#define GPIO_PIN_4   4
-#define GPIO_PIN_5   5
-#define GPIO_PIN_6   6
-#define GPIO_PIN_7   7
+#define GPIO_PIN_0	0
+#define GPIO_PIN_1	1
+#define GPIO_PIN_2	2
+#define GPIO_PIN_3	3
+#define GPIO_PIN_4	4
+#define GPIO_PIN_5	5
+#define GPIO_PIN_6	6
+#define GPIO_PIN_7	7
 
 void GPIO_Init(GPIO_TypeDef *gpio, uint8_t dir);
 void GPIO_Write(GPIO_TypeDef *gpio, uint8_t data);
@@ -32,5 +32,6 @@ uint8_t GPIO_Read(GPIO_TypeDef *gpio);
 void GPIO_Set(GPIO_TypeDef *gpio, uint8_t pinNum);
 void GPIO_Reset(GPIO_TypeDef *gpio, uint8_t pinNum);
 void GPIO_Toggle(GPIO_TypeDef *gpio, uint8_t pinNum);
+int GPIO_ReadPin(GPIO_TypeDef *gpio, uint8_t pinNum);
 
 #endif /* SRC_DEVICE_GPIO_GPIO_H_ */
