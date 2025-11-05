9e84986a018c42a525d5858b59f25b4c4e92ccd9
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-06 08:55:05 +0900

    Update upcounter.h

--- app/upcounter/upcounter.h
+++ app/upcounter/upcounter.h
@@ -4,10 +4,12 @@
 #include "../../driver/fnd/fnd.h"
 #include "../../driver/led/led.h"
 #include "../../driver/common/millis.h"
-
+#include "../../driver/btn/btn.h"
 
 void initUpCounter();
 void runUpCounter();
 void clearUpCounter();
+void exeUpCounter();
+void runDownCounter();
 
 #endif /* SRC_AP_UPCOUNTER_UPCOUNTER_H_ */
