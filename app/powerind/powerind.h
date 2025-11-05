1e698594afa445b23622811257a7b543a8eaa6a5
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-05 11:55:14 +0900

    Create powerind.h

---
+++ app/powerind/powerind.h
@@ -0,0 +1,11 @@
+#ifndef SRC_AP_POWERIND_POWERIND_H_
+#define SRC_AP_POWERIND_POWERIND_H_
+
+#include "../../driver/led/led.h"
+#include "../../driver/common/millis.h"
+#include <stdint.h>
+
+void initPowerInd();
+void dispPowerInd();
+
+#endif /* SRC_AP_POWERIND_POWERIND_H_ */
