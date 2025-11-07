21cfe63872d585d11eb79dff1cc14bbb582a1283
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-07 14:03:14 +0900

    시스템 메인 제어
    시스템 메인 제어 루프와 타이머 인터럽트 초기화 및 처리 루틴을 구현한 애플리케이션 소스로, ap_main()에서 주요 모듈 초기화 후 루프를 실행하고, 인터럽트 컨트롤러(intc_init) 및 타이머(TIMER_IntrInit) 설정,
    인터럽트 서비스 루틴(ISR)에서 FND 갱신을 수행하는 파일입니다.

--- app/ap.c
+++ app/ap.c
@@ -1,53 +1,98 @@
+/*
+ * ap.c
+ *
+ *  Created on: 2025. 11. 4.
+ *      Author: kccistc
+ */
+
+
 #include "ap.h"
 #include "sleep.h"
-
 #include "../driver/btn/btn.h"
-#include "powerind/powerind.h"
-#include "../driver/fnd/fnd.h"
-#include "../driver/btn/btn.h"
-#include "upcounter/upcounter.h"
-
-hLed powerLed;
-hLed upLed;
-hLed downLed;
-hLed togLed;
+#include "../driver/common/millis.h"
+#include "../device/timer/timer.h"
+#include "xintc.h"
+#include "xil_exception.h"
+#include "xparameters.h"
 
 hButton upBtn;
+hLed led0;
 
-void ap_main(){
-   LED_Init(&powerLed, LED_GPIO, LED_0);
-   LED_Init(&upLed, LED_GPIO, LED_1);
-   LED_Init(&downLed, LED_GPIO, LED_2);
-   LED_Init(&togLed, LED_GPIO, LED_3);
+void intc_init();
+void TIMER_IntrInit();
+void ISR();
 
-   FND_Init();
-   Button_Init(&upBtn, GPIOC, GPIO_PIN_0);
-   initPowerInd();
-   initUpCounter();
-
-   while(1)
-   {
-      dispPowerInd();
-//      runUpCounter();
-      //btnLedOn();
-//      if(Button_getState(&upBtn)==ACT_RELEASED){
-//         LED_Toggle(&powerLed);
-//      }
-      exeUpCounter();
-      ISR();
-   }
-}
+XIntc intc;
 
 
-void ISR()
+void ap_main()
 {
-   millisCounter();
 
-   FND_DispNumber();
+
+	initPowerInd();
+	initUpCounter();
+
+
+
+	Millis_Init(TMR1);
+	intc_init();
+	TIMER_IntrInit();
+
+	while(1)
+	{
+
+		dispPowerInd();
+		exeUpCounter();
+
+	}
 }
+
+void intc_init()
+{
+	int status = XIntc_Initialize(&intc, XPAR_INTC_0_DEVICE_ID);
+	if(status == XST_SUCCESS)
+	{
+		xil_printf("INTC Init Successful!\n");
+	} else {
+		xil_printf("INTC Init Fail!\n");
+	}
+	status = XIntc_SelfTest(&intc);
+	if(status == XST_SUCCESS)
+	{
+		xil_printf("INTC SelfTest Successful!\n");
+	} else {
+		xil_printf("INTC SelfTest Fail!\n");
+	}
+
+	///////////////////////////////////////////////////////////////
+	// Initailize and Enable Exception handler
+	//////////////////////////////////////////////////////////////
+	Xil_ExceptionInit();
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XIntc_InterruptHandler, &intc);
+	Xil_ExceptionEnable();
+
+	// function pointer connect
+	XIntc_Connect(&intc, XPAR_INTC_0_TIMER_0_VEC_ID, (XInterruptHandler)ISR, 0);
+	XIntc_Enable(&intc, XPAR_INTC_0_TIMER_0_VEC_ID);
+
+	// enable global interrupt
+	XIntc_Start(&intc, XIN_REAL_MODE);
+}
+
+void TIMER_IntrInit()
+{
+	TMR_SetPSC(TMR0, 100);
+	TMR_SetARR(TMR0, 1000);
+	TMR_Start(TMR0);
+	TMR_IntrStart(TMR0);
+}
+void ISR()	// Interrupt Service Routine
+{
+	FND_DispNumber();
+}
+
 
 void millisCounter()
 {
-   incMillis();
-   usleep(1000);
+	incMillis();
 }
