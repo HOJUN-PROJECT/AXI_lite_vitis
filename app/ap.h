6c28830343a4f29714877d025057ef5dae3af238
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-07 14:03:56 +0900

    동작 함수에 쓰이는 h 선언
    애플리케이션 실행의 진입점인 ap_main() 함수 선언과 함께, LED·FND·타이머·전원 표시기(powerInd)·업카운터(upCounter) 등의 상위 모듈 헤더를 포함한 메인 애플리케이션 헤더 파일입니다.

--- app/ap.h
+++ app/ap.h
@@ -1,15 +1,20 @@
+/*
+ * ap.h
+ *
+ *  Created on: 2025. 11. 4.
+ *      Author: kccistc
+ */
+
+
 #ifndef SRC_AP_AP_H_
 #define SRC_AP_AP_H_
 
 #include "../driver/led/led.h"
 #include "../driver/fnd/fnd.h"
 #include "../driver/common/millis.h"
-#include "powerind/powerind.h"
+#include "powerInd/powerInd.h"
 #include "upCounter/upCounter.h"
-#include "../driver/btn/btn.h"
 
 void ap_main();
-void millisCounter();
-void ISR();
 
 #endif /* SRC_AP_AP_H_ */
