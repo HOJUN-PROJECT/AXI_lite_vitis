86a78603a702a33dd7a3dc8bf6c64164a827fee0
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-07 13:59:29 +0900

    밀리초 함수 프로토타입 정으
    타이머를 이용해 밀리초 단위 시스템 시간을 관리하기 위한 함수들의 선언을 포함한 헤더 파일로, 타이머 초기화 및 시간 제어용 함수 프로토타입을 정의합니다.

--- driver/millis/millis.h
+++ driver/millis/millis.h
@@ -1,10 +1,21 @@
+/*
+ * miillis.h
+ *
+ *  Created on: 2025. 11. 5.
+ *      Author: kccistc
+ */
+
 #ifndef SRC_DRIVER_COMMON_MILLIS_H_
 #define SRC_DRIVER_COMMON_MILLIS_H_
-#include <stdint.h>
 
+#include <stdint.h>
+#include "../../device/timer/timer.h"
+
+void Millis_Init(Timer_TypeDef *tmr);
 void incMillis();
 void clearMillis();
-void seteMillis(uint32_t t);
+void setMillis(uint32_t t);
 uint32_t millis();
+
 
 #endif /* SRC_DRIVER_COMMON_MILLIS_H_ */
