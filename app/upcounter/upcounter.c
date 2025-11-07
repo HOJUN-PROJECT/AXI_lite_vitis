540e41673295636148bf9f8b2945d7628df53357
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-07 14:04:25 +0900

    카운터 동작
    상승·하강 카운터 기능을 구현한 소스로, 버튼 입력에 따라 동작 상태(정지·증가·감소·초기화)를 전환하고, FND에 현재 값을 표시하며 LED로 상태를 시각화하는 기능을 포함한 파일입니다.

--- app/upcounter/upcounter.c
+++ app/upcounter/upcounter.c
@@ -1,60 +1,78 @@
+/*
+ * upCounter.c
+ *
+ *  Created on: 2025. 11. 5.
+ *      Author: kccistc
+ */
+
 #include "upCounter.h"
 
-enum {STOP, UP, CLEAR, DOWN};
+enum {STOP, UP, DOWN, CLEAR};
 
 int upCounterState = STOP;
+
 int counter = 0;
 
 hLed upLed;
 hLed downLed;
-hButton btnStop;
-hButton btnUP;
+hLed stopLed;
+
+hButton btnUp;
 hButton btnClear;
 hButton btnDown;
+hButton btnStop;
 
 void initUpCounter()
 {
-	upCounterState = STOP;
-	counter = 0;
+	//	int upCounterState = STOP;
+	//	int counter = 0;
 	FND_Init();
-	LED_Init(&upLed, LED_GPIO, LED_1);
-	LED_Init(&downLed, LED_GPIO, LED_2);
+	LED_Init(&upLed,	LED_GPIO, LED_1);
+	LED_Init(&downLed,  LED_GPIO, LED_2);
+	LED_Init(&stopLed,  LED_GPIO, LED_3);
 
-	Button_Init(&btnStop, BTN_GPIO, BTN_0);
-	Button_Init(&btnClear, BTN_GPIO, BTN_1);
-	Button_Init(&btnUP, BTN_GPIO, BTN_2);
-	Button_Init(&btnDown, BTN_GPIO, BTN_3);
+	Button_Init(&btnUp, BUTTON_GPIO, BUTTON_0);			// UP Button
+	Button_Init(&btnClear, BUTTON_GPIO, BUTTON_1);		// Left Button
+	Button_Init(&btnStop, BUTTON_GPIO, BUTTON_2);		// Right Button
+	Button_Init(&btnDown, BUTTON_GPIO, BUTTON_3);		// Down Button
 }
 
 void exeUpCounter()
 {
-	switch (upCounterState)
+	switch(upCounterState)
 	{
 	case STOP:
-		if (Button_getState(&btnUP) == ACT_PUSHED) {
+		StopCounter();
+		if(Button_getState(&btnUp) == ACT_PUSHED){
 			upCounterState = UP;
-		} else if (Button_getState(&btnClear) == ACT_PUSHED) {
-			upCounterState = CLEAR;
-		} else if (Button_getState(&btnDown) == ACT_PUSHED) {
+		}
+		else if(Button_getState(&btnDown) == ACT_PUSHED){
 			upCounterState = DOWN;
 		}
+		else if(Button_getState(&btnClear) == ACT_PUSHED){
+			upCounterState = CLEAR;
+		}
 		break;
+
 	case UP:
 		runUpCounter();
-		if (Button_getState(&btnStop) == ACT_PUSHED) {
-					upCounterState = STOP;
-		} else if (Button_getState(&btnDown) == ACT_PUSHED) {
+		if(Button_getState(&btnStop) == ACT_PUSHED){
+			upCounterState = STOP;
+		}
+		else if(Button_getState(&btnDown) == ACT_PUSHED){
 			upCounterState = DOWN;
 		}
 		break;
 	case DOWN:
 		runDownCounter();
-		if (Button_getState(&btnStop) == ACT_PUSHED) {
+		if(Button_getState(&btnStop) == ACT_PUSHED){
 			upCounterState = STOP;
-		} else if (Button_getState(&btnUP) == ACT_PUSHED) {
+		}
+		else if(Button_getState(&btnUp) == ACT_PUSHED){
 			upCounterState = UP;
 		}
 		break;
+
 	case CLEAR:
 		clearUpCounter();
 		upCounterState = STOP;
@@ -66,33 +84,52 @@ void runUpCounter()
 {
 	static uint32_t prevTime = 0;
 	uint32_t curTime = millis();
-	if (curTime - prevTime < 100) return;
+	if(curTime - prevTime < 100) return;
 	prevTime = curTime;
 
-	FND_SetNumber(counter++);
-
-	LED_Off(&downLed);
+	FND_SetNumber(counter);
+	if(counter == 9999){
+		counter = 0;
+	} else{
+		counter++;
+	}
 	LED_Toggle(&upLed);
+	LED_Off(&downLed);
+	LED_Off(&stopLed);
 }
 
 void runDownCounter()
 {
 	static uint32_t prevTime = 0;
 	uint32_t curTime = millis();
-	if (curTime - prevTime < 100) return;
+	if(curTime - prevTime < 100) return;
 	prevTime = curTime;
 
 	FND_SetNumber(counter);
-	if (counter == 0)
-	    {
-	        counter = 9999;
-	    }
-	    else
-	    {
-	        counter--;
-	    }
-	LED_Off(&upLed);
+
+	if(counter == 0){
+		counter = 9999;
+	} else{
+		counter--;
+	}
+	counter = counter;
 	LED_Toggle(&downLed);
+	LED_Off(&upLed);
+	LED_Off(&stopLed);
+}
+
+void StopCounter()
+{
+	static uint32_t prevTime = 0;
+	uint32_t curTime = millis();
+	if(curTime - prevTime < 100) return;
+	prevTime = curTime;
+
+	FND_SetNumber(counter);
+
+	LED_Toggle(&stopLed);
+	LED_Off(&upLed);
+	LED_Off(&downLed);
 }
 
 void clearUpCounter()
