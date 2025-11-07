778fb6e714650ddbfb2143147b645e2cda5f44df
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-11-07 13:58:55 +0900

    밀리초 동작 처리
    시스템 시간을 밀리초 단위로 관리하기 위한 타이머 기반 유틸리티로, 타이머 초기화(Millis_Init), 시간 증가/초기화/설정(incMillis, clearMillis, setMillis), 현재 시간 반환(millis) 기능을 제공하는 파일입니다.

--- driver/millis/millis.c
+++ driver/millis/millis.c
@@ -1,24 +1,40 @@
+/*
+ * millis.c
+ *
+ *  Created on: 2025. 11. 5.
+ *      Author: kccistc
+ */
+
 #include "millis.h"
-#include <stdint.h>
 
 uint32_t m_counter = 0;
+Timer_TypeDef *millis_timer;
+
+void Millis_Init(Timer_TypeDef *tmr)
+{
+	millis_timer = tmr;
+	TMR_SetPSC(millis_timer, 100000);
+	TMR_SetARR(millis_timer, 0xffffffff);
+	TMR_Start(millis_timer);
+}
 
 void incMillis()
 {
-   m_counter++;
+	m_counter++;
 }
 
 void clearMillis()
 {
-   m_counter = 0;
+	m_counter = 0;
 }
 
-void seteMillis(uint32_t t)
+void setMillis(uint32_t t)
 {
-   m_counter = t;
+	m_counter = t;
 }
 
 uint32_t millis()
 {
-   return m_counter;
+	//return m_counter;
+	return TMR_ReadCNT(millis_timer);
 }
